4e5216e071b2b812a397038a4ea405997db30337
jeldon123 <jelsasukeops@gmail.com>
2017-03-08 09:46:11 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
+const token = process.env.FB_VERIFY_TOKEN
+const access = process.env.FB_VERIFY_TOKEN
+
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -34,28 +37,106 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-      let event = req.body.entry[0].messaging[i]
-      let sender = event.sender.id
-      if (event.message && event.message.text) {
-        let text = event.message.text
-        if (text === 'Generic') {
-            sendGenericMessage(sender)
-            continue
-        }
-        sendTextMessage(sender, "Message received: " + text.substring(0, 200))
-      }
-      if (event.postback) {
-        let text = JSON.stringify(event.postback)
-        sendTextMessage(sender, "Postback: "+text.substring(0, 200), token)
-        continue
-      }
-    }
-    res.sendStatus(200)
-  })
+app.post('/webhook', function (req, res) {
+  var data = req.body;
 
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+   var recipientID = event.recipient.id;
+   var timeOfMessage = event.timestamp;
+   var message = event.message;
+
+   console.log("Received message for user %d and page %d at %d with message:",
+     senderID, recipientID, timeOfMessage);
+   console.log(JSON.stringify(message));
+
+   var messageId = message.mid;
+
+   var messageText = message.text;
+   var messageAttachments = message.attachments;
+
+   if (messageText) {
+
+     // If we receive a text message, check to see if it matches a keyword
+     // and send back the example. Otherwise, just echo the text we received.
+     switch (messageText) {
+       case 'generic':
+         sendGenericMessage(senderID);
+         break;
+
+       default:
+         sendTextMessage(senderID, messageText);
+     }
+   } else if (messageAttachments) {
+     sendTextMessage(senderID, "Message with attachment received");
+   }
+}
+
+function sendGenericMessage(recipientId, messageText) {
+  // To be expanded in later sections
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: access },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
 
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
